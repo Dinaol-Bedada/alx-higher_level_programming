8dce625159298846d3c2b39760257d5d18729fa5
Dinaol Bedada <117851922+Dinaol-Bedada@users.noreply.github.com>
2023-04-04 22:52:40 +0300

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-""" This is the "LockedClass module """
-
-
 class LockedClass:
     __slots__ = ['first_name']
+    
+    def __setattr__(self, key, value):
+        if key != 'first_name' or not hasattr(self, 'first_name'):
+            raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{key}'")
+        super().__setattr__(key, value)
