3817b731d647a937f15ad6cd962c4f2829eba8af
Dinaol Bedada <117851922+Dinaol-Bedada@users.noreply.github.com>
2023-04-04 22:51:27 +0300

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-    """
-    Definition for a LockedClass class.
-    """
+""" This is the "LockedClass module """
 
 
-    class LockedClass:
-        """LockedClass class that defines 1 attribute"""
-
-        __slots__ = ['first_name']
+class LockedClass:
+    __slots__ = ['first_name']
